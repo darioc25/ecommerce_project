171d6c215ece0622dd7c9affc2e32cc2faf02b63
Vasile Dario Coman <dario.25@live.it>
2024-05-21 13:44:21 +0200

    Fixed menu section logic and setted animation on menu button, added section products list.

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 
 const API_URL = "https://dummyjson.com/products?limit=0";
 
+const mdBreakPoint = 768;
+
 const menuBtn = document.querySelector(".navbar-menu-btn");
 const sectionFilter = document.querySelector(".section-filter");
+const sectionListFilter = document.querySelector(".section-list-filter");
 
 const example = {
    id: 1,
@@ -34,6 +37,27 @@ const getProducts = async function() {
 
 // getProducts();
 
+let menuBtnFlag = false;
 menuBtn.addEventListener("click", () => {
-   sectionFilter.style.left = "0%";
+   const position = sectionFilter.style.left;
+   const bodyWidth = document.body.clientWidth;
+   if(menuBtnFlag) {
+      menuBtn.style.transform = "rotate(0deg)";
+      menuBtnFlag = false;
+   } else {
+      menuBtn.style.transform = "rotate(90deg)";
+      menuBtnFlag = true;
+   };
+   if(!position || position !== "0%") {
+      sectionFilter.style.left = "0%";
+      sectionListFilter.style.opacity = "50%";
+   } else if(position && bodyWidth >= mdBreakPoint) {
+      sectionFilter.style.left = "-50%";
+      sectionListFilter.style.opacity = "0%";
+      sectionFilter.style.removeProperty("left");
+   } else if(position && bodyWidth < mdBreakPoint) {
+      sectionFilter.style.left = "-100%"
+      sectionListFilter.style.opacity = "0%";
+      sectionFilter.style.removeProperty("left");
+   }
 });
